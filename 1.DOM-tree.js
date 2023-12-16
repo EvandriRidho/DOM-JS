1ab36f6d0ed37558b50810b329694517f588ec0a
EvandriRidho <152156672+EvandriRidho@users.noreply.github.com>
2023-12-16 18:08:17 +0700

    Add files via upload

---
+++ 1.DOM-tree.js
@@ -0,0 +1,23 @@
+/* <div id="container">
+        <h1>Selamat Datang</h1>
+        <p>Ini adalah contoh dokumen HTML dengan DOM.</p>
+    </div>  */
+
+// Dom tree
+/*- Document (Element DOM)
+    - html
+        - head
+            - title
+                - "Contoh DOM" (Teks DOM)
+        - body
+            - div id="container" (id sebuah Atribut)
+                - h1
+                    - "Selamat Datang"
+                - p
+                    - "Ini adalah contoh dokumen HTML dengan DOM." */
+
+// Mengakses elemen <p> dengan id "container"
+var paragraph = document.getElementById("container").getElementsByTagName("p")[0];
+
+// Mengubah teks dalam elemen <p>
+paragraph.innerHTML = "Ini adalah teks yang diubah melalui JavaScript.";
